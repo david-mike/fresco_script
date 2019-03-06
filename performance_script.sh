e225ec024a27ca3d58f490af47d0fad07aade35f
root <root@sparkmaster.ecn.purdue.edu>
2019-03-05 14:14:50 -1000

    add the performance filter script

---
+++ performance_script.sh
@@ -0,0 +1,138 @@
+#python3.7 performance_filter.py 2015-03-filtered.csv 2015-03/cpu.csv
+#python3.7 performance_filter.py 2015-03-filtered.csv 2015-03/mem.csv
+#python3.7 performance_filter.py 2015-03-filtered.csv 2015-03/block.csv
+
+#python3.7 performance_filter.py 2015-04-filtered.csv 2015-04/ps.csv
+#python3.7 performance_filter.py 2015-04-filtered.csv 2015-04/cpu.csv
+#python3.7 performance_filter.py 2015-04-filtered.csv 2015-04/mem.csv
+#python3.7 performance_filter.py 2015-04-filtered.csv 2015-04/block.csv
+
+#python3.7 performance_filter.py 2015-05-filtered.csv 2015-05/ps.csv
+#python3.7 performance_filter.py 2015-05-filtered.csv 2015-05/cpu.csv
+#python3.7 performance_filter.py 2015-05-filtered.csv 2015-05/mem.csv
+#python3.7 performance_filter.py 2015-05-filtered.csv 2015-05/block.csv
+
+#python3.7 performance_filter.py 2015-06-filtered.csv 2015-06/ps.csv
+#python3.7 performance_filter.py 2015-06-filtered.csv 2015-06/cpu.csv
+#python3.7 performance_filter.py 2015-06-filtered.csv 2015-06/mem.csv
+#python3.7 performance_filter.py 2015-06-filtered.csv 2015-06/block.csv
+
+#python3.7 performance_filter.py 2015-07-filtered.csv 2015-07/ps.csv
+#python3.7 performance_filter.py 2015-07-filtered.csv 2015-07/cpu.csv
+#python3.7 performance_filter.py 2015-07-filtered.csv 2015-07/mem.csv
+#python3.7 performance_filter.py 2015-07-filtered.csv 2015-07/block.csv
+
+#python3.7 performance_filter.py 2015-08-filtered.csv 2015-08/ps.csv
+#python3.7 performance_filter.py 2015-08-filtered.csv 2015-08/cpu.csv
+#python3.7 performance_filter.py 2015-08-filtered.csv 2015-08/mem.csv
+#python3.7 performance_filter.py 2015-08-filtered.csv 2015-08/block.csv
+
+#python3.7 performance_filter.py 2015-09-filtered.csv 2015-09/ps.csv
+#python3.7 performance_filter.py 2015-09-filtered.csv 2015-09/cpu.csv
+#python3.7 performance_filter.py 2015-09-filtered.csv 2015-09/mem.csv
+#python3.7 performance_filter.py 2015-09-filtered.csv 2015-09/block.csv
+
+#python3.7 performance_filter.py 2015-10-filtered.csv 2015-10/ps.csv
+#python3.7 performance_filter.py 2015-10-filtered.csv 2015-10/cpu.csv
+#python3.7 performance_filter.py 2015-10-filtered.csv 2015-10/mem.csv
+#python3.7 performance_filter.py 2015-10-filtered.csv 2015-10/block.csv
+
+#python3.7 performance_filter.py 2015-11-filtered.csv 2015-11/ps.csv
+#python3.7 performance_filter.py 2015-11-filtered.csv 2015-11/cpu.csv
+#python3.7 performance_filter.py 2015-11-filtered.csv 2015-11/mem.csv
+#python3.7 performance_filter.py 2015-11-filtered.csv 2015-11/block.csv
+
+#python3.7 performance_filter.py 2015-12-filtered.csv 2015-12/ps.csv
+#python3.7 performance_filter.py 2015-12-filtered.csv 2015-12/cpu.csv
+#python3.7 performance_filter.py 2015-12-filtered.csv 2015-12/mem.csv
+#python3.7 performance_filter.py 2015-12-filtered.csv 2015-12/block.csv
+
+#python3.7 performance_filter.py 2016-01-filtered.csv 2016-01/ps.csv
+#python3.7 performance_filter.py 2016-01-filtered.csv 2016-01/cpu.csv
+#python3.7 performance_filter.py 2016-01-filtered.csv 2016-01/mem.csv
+#python3.7 performance_filter.py 2016-01-filtered.csv 2016-01/block.csv
+
+#python3.7 performance_filter.py 2016-02-filtered.csv 2016-02/ps.csv
+#python3.7 performance_filter.py 2016-02-filtered.csv 2016-02/cpu.csv
+#python3.7 performance_filter.py 2016-02-filtered.csv 2016-02/mem.csv
+#python3.7 performance_filter.py 2016-02-filtered.csv 2016-02/block.csv
+
+#python3.7 performance_filter.py 2016-03-filtered.csv 2016-03/ps.csv
+#python3.7 performance_filter.py 2016-03-filtered.csv 2016-03/cpu.csv
+#python3.7 performance_filter.py 2016-03-filtered.csv 2016-03/mem.csv
+#python3.7 performance_filter.py 2016-03-filtered.csv 2016-03/block.csv
+
+#python3.7 performance_filter.py 2016-04-filtered.csv 2016-04/ps.csv
+#python3.7 performance_filter.py 2016-04-filtered.csv 2016-04/cpu.csv
+#python3.7 performance_filter.py 2016-04-filtered.csv 2016-04/mem.csv
+#python3.7 performance_filter.py 2016-04-filtered.csv 2016-04/block.csv
+
+#python3.7 performance_filter.py 2016-05-filtered.csv 2016-05/ps.csv
+#python3.7 performance_filter.py 2016-05-filtered.csv 2016-05/cpu.csv
+#python3.7 performance_filter.py 2016-05-filtered.csv 2016-05/mem.csv
+#python3.7 performance_filter.py 2016-05-filtered.csv 2016-05/block.csv
+
+#python3.7 performance_filter.py 2016-06-filtered.csv 2016-06/ps.csv
+#python3.7 performance_filter.py 2016-06-filtered.csv 2016-06/cpu.csv
+#python3.7 performance_filter.py 2016-06-filtered.csv 2016-06/mem.csv
+#python3.7 performance_filter.py 2016-06-filtered.csv 2016-06/block.csv
+
+#python3.7 performance_filter.py 2016-07-filtered.csv 2016-07/ps.csv
+#python3.7 performance_filter.py 2016-07-filtered.csv 2016-07/cpu.csv
+#python3.7 performance_filter.py 2016-07-filtered.csv 2016-07/mem.csv
+#python3.7 performance_filter.py 2016-07-filtered.csv 2016-07/block.csv
+
+#python3.7 performance_filter.py 2016-08-filtered.csv 2016-08/ps.csv
+#python3.7 performance_filter.py 2016-08-filtered.csv 2016-08/cpu.csv
+#python3.7 performance_filter.py 2016-08-filtered.csv 2016-08/mem.csv
+#python3.7 performance_filter.py 2016-08-filtered.csv 2016-08/block.csv
+
+#python3.7 performance_filter.py 2016-09-filtered.csv 2016-09/ps.csv
+#python3.7 performance_filter.py 2016-09-filtered.csv 2016-09/cpu.csv
+#python3.7 performance_filter.py 2016-09-filtered.csv 2016-09/mem.csv
+#python3.7 performance_filter.py 2016-09-filtered.csv 2016-09/block.csv
+
+#python3.7 performance_filter.py 2016-10-filtered.csv 2016-10/ps.csv
+#python3.7 performance_filter.py 2016-10-filtered.csv 2016-10/cpu.csv
+#python3.7 performance_filter.py 2016-10-filtered.csv 2016-10/mem.csv
+#python3.7 performance_filter.py 2016-10-filtered.csv 2016-10/block.csv
+
+#python3.7 performance_filter.py 2016-11-filtered.csv 2016-11/ps.csv
+#python3.7 performance_filter.py 2016-11-filtered.csv 2016-11/cpu.csv
+python3.7 performance_filter.py 2016-11-filtered.csv 2016-11/mem.csv
+python3.7 performance_filter.py 2016-11-filtered.csv 2016-11/block.csv
+
+python3.7 performance_filter.py 2016-12-filtered.csv 2016-12/ps.csv
+python3.7 performance_filter.py 2016-12-filtered.csv 2016-12/cpu.csv
+python3.7 performance_filter.py 2016-12-filtered.csv 2016-12/mem.csv
+python3.7 performance_filter.py 2016-12-filtered.csv 2016-12/block.csv
+
+python3.7 performance_filter.py 2017-01-filtered.csv 2017-01/ps.csv
+python3.7 performance_filter.py 2017-01-filtered.csv 2017-01/cpu.csv
+python3.7 performance_filter.py 2017-01-filtered.csv 2017-01/mem.csv
+python3.7 performance_filter.py 2017-01-filtered.csv 2017-01/block.csv
+
+python3.7 performance_filter.py 2017-02-filtered.csv 2017-02/ps.csv
+python3.7 performance_filter.py 2017-02-filtered.csv 2017-02/cpu.csv
+python3.7 performance_filter.py 2017-02-filtered.csv 2017-02/mem.csv
+python3.7 performance_filter.py 2017-02-filtered.csv 2017-02/block.csv
+
+python3.7 performance_filter.py 2017-03-filtered.csv 2017-03/ps.csv
+python3.7 performance_filter.py 2017-03-filtered.csv 2017-03/cpu.csv
+python3.7 performance_filter.py 2017-03-filtered.csv 2017-03/mem.csv
+python3.7 performance_filter.py 2017-03-filtered.csv 2017-03/block.csv
+
+python3.7 performance_filter.py 2017-04-filtered.csv 2017-04/ps.csv
+python3.7 performance_filter.py 2017-04-filtered.csv 2017-04/cpu.csv
+python3.7 performance_filter.py 2017-04-filtered.csv 2017-04/mem.csv
+python3.7 performance_filter.py 2017-04-filtered.csv 2017-04/block.csv
+
+python3.7 performance_filter.py 2017-05-filtered.csv 2017-05/ps.csv
+python3.7 performance_filter.py 2017-05-filtered.csv 2017-05/cpu.csv
+python3.7 performance_filter.py 2017-05-filtered.csv 2017-05/mem.csv
+python3.7 performance_filter.py 2017-05-filtered.csv 2017-05/block.csv
+
+python3.7 performance_filter.py 2017-06-filtered.csv 2017-06/ps.csv
+python3.7 performance_filter.py 2017-06-filtered.csv 2017-06/cpu.csv
+python3.7 performance_filter.py 2017-06-filtered.csv 2017-06/mem.csv
+python3.7 performance_filter.py 2017-06-filtered.csv 2017-06/block.csv
